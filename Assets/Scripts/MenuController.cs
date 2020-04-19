39d7783609508a7e9a48189f26330137def9d61f
Benjamin Frye <ben.j.frye@gmail.com>
2020-04-19 13:11:57 +1000

    Add main menu.

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public string levelName = "Level0";
+    public void Play()
+    {
+        SceneManager.LoadScene(levelName, LoadSceneMode.Single);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
